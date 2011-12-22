30006905b250bae22589d30b2e2904a0810b520f
Ben Huson <ben@thewhiteroom.net>
2011-12-22 07:08:49 +0000

    Move functionality to class and code tidy.

--- content-parts.php
+++ content-parts.php
@@ -1,9 +1,6 @@
 <?php
 
-
-
 /*
-
 Plugin Name: Content Parts
 Plugin URI: http://www.benhuson.co.uk/wordpress-plugins/content-parts/
 Description: Divide your post content into sections.
@@ -11,99 +8,86 @@ Version: 1.2
 Author: Ben Huson
 Author URI: http://www.benhuson.co.uk
 License: GPL
-
 */
 
-
-
-// Admin Includes
-if ( is_admin() ) {
-	include_once( WP_PLUGIN_DIR . '/content-parts/admin/editor.php' );
-}
-
-
-
-/**
- * content_parts_query_vars
- */
-
-function content_parts_query_vars( ) {
+class Content_Parts {
 	
-	global $num_content_parts, $content_parts, $post;
+	var $editor = '';
 	
-	if ( !isset( $post ) )
-		return;
+	/**
+	 * Constructor
+	 */
+	function Content_Parts() {
+		add_action( 'wp', array( $this, 'content_parts_query_vars' ) );
+		
+		// Admin Includes
+		if ( is_admin() ) {
+			include_once( WP_PLUGIN_DIR . '/content-parts/admin/editor.php' );
+			$this->editor = new Content_Parts_Editor();
+		}
+	}
 	
-	$content = $post->post_content;
-	if ( strpos( $content, '<!--contentpartdivider-->' ) ) {
-		$content = str_replace("\n<!--contentpartdivider-->\n", '<!--contentpartdivider-->', $content);
-		$content = str_replace("\n<!--contentpartdivider-->", '<!--contentpartdivider-->', $content);
-		$content = str_replace("<!--contentpartdivider-->\n", '<!--contentpartdivider-->', $content);
-		$content_parts = explode('<!--contentpartdivider-->', $content);
-		$num_content_parts = count( $content_parts );
-	} else {
-		$content_parts = array( $post->post_content );
-		$num_content_parts = 1;
+	/**
+	 * content_parts_query_vars
+	 */
+	function content_parts_query_vars( ) {
+		global $num_content_parts, $content_parts, $post;
+		
+		if ( !isset( $post ) )
+			return;
+		
+		$content = $post->post_content;
+		if ( strpos( $content, '<!--contentpartdivider-->' ) ) {
+			$content = str_replace("\n<!--contentpartdivider-->\n", '<!--contentpartdivider-->', $content);
+			$content = str_replace("\n<!--contentpartdivider-->", '<!--contentpartdivider-->', $content);
+			$content = str_replace("<!--contentpartdivider-->\n", '<!--contentpartdivider-->', $content);
+			$content_parts = explode('<!--contentpartdivider-->', $content);
+			$num_content_parts = count( $content_parts );
+		} else {
+			$content_parts = array( $post->post_content );
+			$num_content_parts = 1;
+		}
 	}
 	
 }
 
-add_action( 'wp', 'content_parts_query_vars' );
+global $Content_Parts;
+global $num_content_parts, $content_parts, $post;
+$Content_Parts = new Content_Parts();
 
 
 
 /**
- * @method       The Content Part
- * @description  Displays the content of a paged page.
- * @output       The page content HTML
+ * The Content Part
+ * Displays the content of a paged page.
  */
-
 function the_content_part( $page = 1, $before = '', $after = '' ) {
-	
 	$output = get_the_content_part( $page );
-	
 	if ( !empty( $output ) ) {
 		echo $before . $output . $after;
 	}
-	
 }
 
-
-
 /**
- * @method       Get The Content Part
- * @description  Returns the content of a paged page.
- * @returns      (String) The page content HTML
+ * Get The Content Part
+ * Returns the content of a paged page.
  */
-
 function get_the_content_part( $page = 1 ) {
-	
 	global $content_parts;
-	
 	$page = absint($page);
-	
 	if ( $page > 0 && $page <= count( $content_parts ) ) {
-	
 		$content = force_balance_tags( $content_parts[$page - 1] );
 		$content = apply_filters( 'the_content', $content );
 		return $content;
-	
 	}
-	
 	return '';
-	
 }
 
-
-
 /**
- * @method       The Content Parts
- * @description  Displays all content of a paged page.
- * @output       The page content HTML
+ * The Content Parts
+ * Displays all content of a paged page.
  */
-
 function the_content_parts( $args = null ) {
-	
 	global $content_parts;
 
 	$defaults = array(
@@ -112,7 +96,6 @@ function the_content_parts( $args = null ) {
 		'start'  => 1,
 		'limit'  => 0
 	);
-	
 	$pargs = wp_parse_args( $args, $defaults );
 	
 	$pargs['start'] = absint( $pargs['start'] );
@@ -129,61 +112,36 @@ function the_content_parts( $args = null ) {
 		}
 		$count++;
 	}
-	
 }
 
-
-
 /**
- * @method       Get The Content Parts
- * @description  Returns an array of the content of a paged page.
- * @returns      (Array) The page content HTML
+ * Get The Content Parts
+ * Returns an array of the content of a paged page.
  */
-
 function get_the_content_parts() {
-	
 	global $content_parts;
-	
 	return $content_parts;
-	
 }
 
-
-
 /**
- * @method       Has Content Parts
- * @description  Returns true/false depending if there are multiple content parts.
- * @returns      (Boolean)
+ * Has Content Parts
+ * Returns true/false depending if there are multiple content parts.
  */
-
 function has_content_parts() {
-	
 	global $content_parts;
-	
 	if ( count( $content_parts ) > 1 ) {
 		return true;
 	}
-	
 	return false;
-	
 }
-
-
 
 /**
-* @method Count Content Parts
-* @description Returns count of available content parts.
-* @returns (Int)
-*/
-
+ * Count Content Parts
+ * Returns count of available content parts.
+ */
 function count_content_parts() {
-	
 	global $content_parts;
-	
 	return count( $content_parts );
-	
 }
-
-
 
 ?>
