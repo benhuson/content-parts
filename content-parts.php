e121e0532318f23983152a3141a11041a60f914f
Ben Huson <ben@thewhiteroom.net>
2011-12-22 07:20:20 +0000

    Move all functionality to class.

--- content-parts.php
+++ content-parts.php
@@ -49,99 +49,133 @@ class Content_Parts {
 		}
 	}
 	
+	/**
+	 * The Content Part
+	 * Displays the content of a paged page.
+	 */
+	function the_content_part( $page = 1, $before = '', $after = '' ) {
+		$output = get_the_content_part( $page );
+		if ( !empty( $output ) ) {
+			echo $before . $output . $after;
+		}
+	}
+	
+	/**
+	 * Get The Content Part
+	 * Returns the content of a paged page.
+	 */
+	function get_the_content_part( $page = 1 ) {
+		global $content_parts;
+		$page = absint($page);
+		if ( $page > 0 && $page <= count( $content_parts ) ) {
+			$content = force_balance_tags( $content_parts[$page - 1] );
+			$content = apply_filters( 'the_content', $content );
+			return $content;
+		}
+		return '';
+	}
+	
+	/**
+	 * The Content Parts
+	 * Displays all content of a paged page.
+	 */
+	function the_content_parts( $args = null ) {
+		global $content_parts;
+	
+		$defaults = array(
+			'before' => '',
+			'after'  => '',
+			'start'  => 1,
+			'limit'  => 0
+		);
+		$pargs = wp_parse_args( $args, $defaults );
+		
+		$pargs['start'] = absint( $pargs['start'] );
+		if ( $pargs['start'] <= 0 )
+			$pargs['start'] = 1;
+		$pargs['limit'] = absint( $pargs['limit'] );
+		
+		$count = 1;
+		foreach ( $content_parts as $page ) {
+			if ( $count >= $pargs['start'] && ( $pargs['limit'] == 0 || $count < $pargs['start'] + $pargs['limit']  ) ) {
+				$content = force_balance_tags( $page );
+				$content = apply_filters( 'the_content', $content );
+				echo $pargs['before'] . $content . $pargs['after'];
+			}
+			$count++;
+		}
+	}
+	
+	/**
+	 * Get The Content Parts
+	 * Returns an array of the content of a paged page.
+	 */
+	function get_the_content_parts() {
+		global $content_parts;
+		return $content_parts;
+	}
+	
+	/**
+	 * Has Content Parts
+	 * Returns true/false depending if there are multiple content parts.
+	 */
+	function has_content_parts() {
+		global $content_parts;
+		if ( count( $content_parts ) > 1 ) {
+			return true;
+		}
+		return false;
+	}
+	
+	/**
+	 * Count Content Parts
+	 * Returns count of available content parts.
+	 */
+	function count_content_parts() {
+		global $content_parts;
+		return count( $content_parts );
+	}
+
 }
 
 global $Content_Parts;
 global $num_content_parts, $content_parts, $post;
 $Content_Parts = new Content_Parts();
 
-
-
-/**
- * The Content Part
- * Displays the content of a paged page.
- */
+// The Content Part
 function the_content_part( $page = 1, $before = '', $after = '' ) {
-	$output = get_the_content_part( $page );
-	if ( !empty( $output ) ) {
-		echo $before . $output . $after;
-	}
+	global $Content_Parts;
+	$Content_Parts->the_content_part( $page, $before, $after );
 }
 
-/**
- * Get The Content Part
- * Returns the content of a paged page.
- */
+// Get The Content Part
 function get_the_content_part( $page = 1 ) {
-	global $content_parts;
-	$page = absint($page);
-	if ( $page > 0 && $page <= count( $content_parts ) ) {
-		$content = force_balance_tags( $content_parts[$page - 1] );
-		$content = apply_filters( 'the_content', $content );
-		return $content;
-	}
-	return '';
+	global $Content_Parts;
+	return $Content_Parts->get_the_content_part( $page );
 }
 
-/**
- * The Content Parts
- * Displays all content of a paged page.
- */
+// The Content Parts
 function the_content_parts( $args = null ) {
-	global $content_parts;
-
-	$defaults = array(
-		'before' => '',
-		'after'  => '',
-		'start'  => 1,
-		'limit'  => 0
-	);
-	$pargs = wp_parse_args( $args, $defaults );
-	
-	$pargs['start'] = absint( $pargs['start'] );
-	if ( $pargs['start'] <= 0 )
-		$pargs['start'] = 1;
-	$pargs['limit'] = absint( $pargs['limit'] );
-	
-	$count = 1;
-	foreach ( $content_parts as $page ) {
-		if ( $count >= $pargs['start'] && ( $pargs['limit'] == 0 || $count < $pargs['start'] + $pargs['limit']  ) ) {
-			$content = force_balance_tags( $page );
-			$content = apply_filters( 'the_content', $content );
-			echo $pargs['before'] . $content . $pargs['after'];
-		}
-		$count++;
-	}
+	global $Content_Parts;
+	$Content_Parts->the_content_parts( $args );
 }
 
-/**
- * Get The Content Parts
- * Returns an array of the content of a paged page.
- */
+// Get The Content Parts
 function get_the_content_parts() {
-	global $content_parts;
-	return $content_parts;
+	global $Content_Parts;
+	return $Content_Parts->get_the_content_parts();
 }
 
-/**
- * Has Content Parts
- * Returns true/false depending if there are multiple content parts.
- */
+// Has Content Parts
 function has_content_parts() {
-	global $content_parts;
-	if ( count( $content_parts ) > 1 ) {
-		return true;
-	}
-	return false;
+	global $Content_Parts;
+	return $Content_Parts->has_content_parts();
 }
 
-/**
- * Count Content Parts
- * Returns count of available content parts.
- */
+// Count Content Parts
 function count_content_parts() {
-	global $content_parts;
-	return count( $content_parts );
+	global $Content_Parts;
+	return $Content_Parts->count_content_parts();
 }
 
 ?>
