dd61b3d0877579d824d25298f4d7b9d80bd991d4
Ben Huson <ben@thewhiteroom.net>
2012-07-10 19:55:48 +0100

    Add admin code documentation.

--- admin/editor.php
+++ admin/editor.php
@@ -17,7 +17,7 @@ class Content_Parts_Editor {
 	}
 	
 	/**
-	 * Add Buttons
+	 * Add Editor Buttons
 	 */
 	function add_buttons() {
 		// Don't bother doing this stuff if the current user lacks permissions
@@ -32,8 +32,12 @@ class Content_Parts_Editor {
 	}
 	
 	/**
-	 * Register Map Button
-	 * This function adds the Content Part button to the editor.
+	 * Register Content Parts Button
+	 *
+	 * This function registers the Content Part button for the editor.
+	 *
+	 * @param array $buttons Buttons array.
+	 * @return array Buttons array.
 	 */
 	function register_content_parts_button( $buttons ) {
 		array_push( $buttons, 'separator', 'contentparts' );
@@ -41,8 +45,12 @@ class Content_Parts_Editor {
 	}
 	
 	/**
-	 * Load TinyMCE Content Parts Plugin
+	 * Add TinyMCE Content Parts Plugin
+	 *
 	 * This function adds the Content Part button to the editor.
+	 *
+	 * @param array $plugin_array TinyMCE plugins array.
+	 * @return array Plugins array.
 	 */
 	function add_content_parts_plugin( $plugin_array ) {
 		$plugin_array['contentparts'] = WP_PLUGIN_URL . '/content-parts/js/tinymce/plugins/contentparts/editor_plugin.js';
